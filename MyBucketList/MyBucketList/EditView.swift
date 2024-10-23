dad26e27dcc29883681ea131d12cf50d52c57877
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-10-23 23:43:20 +0530

    get nearby values

--- MyBucketList/MyBucketList/EditView.swift
+++ MyBucketList/MyBucketList/EditView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 
 struct EditView: View {
     
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     @Environment(\.dismiss) var dismiss
     var location: Location
     @State private var name: String
     @State private var discription: String
     var onSave: (Location) -> Void
+    
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
     
     var body: some View {
         NavigationStack {
@@ -23,11 +30,28 @@ struct EditView: View {
                     TextField("Name", text: $name)
                     TextField("Description", text: $discription)
                 }
+                
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading...")
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(":") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Faild")
+                    }
+                    
+                }
             }
             .navigationTitle("Place details")
             .toolbar {
                 Button("save") {
-                    
                     var newLocation = location
                     newLocation.id = UUID()
                     newLocation.name = name
@@ -35,6 +59,9 @@ struct EditView: View {
                     onSave(newLocation)
                     dismiss()
                 }
+            }
+            .task {
+                await fetchNearbyPlaces()
             }
         }
     }
@@ -46,6 +73,25 @@ struct EditView: View {
         _discription = State(initialValue: location.description)
     }
     
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+    
+        guard let url = URL(string: urlString) else {
+            print("Bad URL...")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            pages = items.query.pages.values.sorted { $0.title <  $1.title }
+            
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
+    }
 }
 
 #Preview {
---
+++ MyBucketList/MyBucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  MyBucketList
+//
+//  Created by Mantosh Kumar on 23/10/24.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
